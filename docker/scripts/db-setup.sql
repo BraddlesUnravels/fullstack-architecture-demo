9b91e81131225bd6cad5b41be2dc60b350f97870
Brad Laskey <tidemaster2@gmail.com>
2026-04-30 23:01:55 +1000

    fixes the duplicate creation of the app_db

--- docker/scripts/db-setup.sql
+++ docker/scripts/db-setup.sql
@@ -1,7 +1,5 @@
 CREATE USER admin WITH PASSWORD 'password';
 
-CREATE DATABASE app_db OWNER admin;
-
 GRANT ALL PRIVILEGES ON DATABASE app_db TO admin;
 
 -- app_user permissions are granted after tables exist
